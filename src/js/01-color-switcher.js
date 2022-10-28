c1a4cad21f864a91fe308b96b3381367ae9844a7
ZaraMelikyan <zaramelikyan1@gmail.com>
2022-10-28 08:48:23 +0300

    1 and 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+let setIntervalID = null;
+const COLOR_CHANGE_DELAY = 1000;
 
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick(event) {
+  setIntervalID = setInterval(bodyChangeColor, COLOR_CHANGE_DELAY);
+
+  event.currentTarget.disabled = true;
+  refs.stopButton.disabled = false;
+}
+
+function onStopButtonClick(event) {
+  clearInterval(setIntervalID);
+
+  event.currentTarget.disabled = true;
+  refs.startButton.disabled = false;
+}
+
+function bodyChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', onFormSubmit);
+
+const resolved = ({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+const rejected = ({ position, delay }) => {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let firstDelay = event.currentTarget.elements.delay.value;
+  let amount = event.currentTarget.elements.amount.value;
+  let delayStep = event.currentTarget.elements.step.value;
+  let totalDelay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    totalDelay = firstDelay;
+    if (i >= 2) {
+      totalDelay = Number(firstDelay) + Number(delayStep * (i - 1));
+    }
+
+    createPromise(i, totalDelay).then(resolved).catch(rejected);
+
+    console.log(`Promise #${i} will be processed after ` + totalDelay + 'ms');
+  }
+
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
